c19f9c4c18d96bf67ac1085765b628c5dfc32ef8
shiyangDR <admin@iisns.com>
2015-02-07 22:25:10 +0800

    Initial commit

---
+++ Login.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace shiyang\login;
+
+/**
+ * LoginWidget is a widget that provides user login functionality.
+ */
+class Login extends \yii\base\Widget
+{
+	/**
+	 * @var string the widget title. Defaults to 'Login'.
+	 */
+	public $title='Login';
+
+	/**
+	 * @var boolean whether the widget is visible. Defaults to true.
+	 */
+	public $visible = true;
+
+    public function run()
+    {
+		if($this->visible) {
+	    	$user = new LoginForm;
+	    	if ($user->load(\Yii::$app->request->post()) && $user->login()) {
+	    		return \Yii::$app->getResponse()->refresh();
+	        } else {
+	            return $this->render('loginWidget', [
+	                'user' => $user,
+	                'title' => $this->title,
+	            ]);
+	        }
+		}
+    }
+}
---
+++ views/loginWidget.php
@@ -0,0 +1,20 @@
+<?php
+use yii\bootstrap\ActiveForm; 
+use yii\helpers\Html;
+?>
+<div class="panel panel-default">
+  <div class="panel-heading"><?= $title ?></div>
+  <div class="panel-body">
+	<?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+	    <?= $form->field($user, 'username')->label(false) ?>
+	    <?= $form->field($user, 'password')->passwordInput()->label(false) ?>
+	    <div style="color:#999;margin:1em 0">
+	        If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
+	    </div>
+	    <div class="form-group">
+	        <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+	        <?= Html::a('Signup', ['/site/signup']) ?>
+	    </div>
+	<?php ActiveForm::end(); ?>
+  </div>
+</div>
