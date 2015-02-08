5451cfde6265af14aaaf27d9fb6abd360a7e663c
shiyangDR <admin@iisns.com>
2015-02-08 22:06:11 +0800

    add placeholder

--- views/loginWidget.php
+++ views/loginWidget.php
@@ -6,8 +6,18 @@ use yii\helpers\Html;
   <div class="panel-heading"><?= $title ?></div>
   <div class="panel-body">
     <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-        <?= $form->field($user, 'username')->label(false) ?>
-        <?= $form->field($user, 'password')->passwordInput()->label(false) ?>
+        <?= $form->field($user, 'username', [
+              'inputOptions' => [
+                  'placeholder' => $user->getAttributeLabel('username'),
+              ],
+            ])->label(false);
+        ?>
+        <?= $form->field($user, 'password', [
+              'inputOptions' => [
+                  'placeholder' => $user->getAttributeLabel('password'),
+              ],
+            ])->passwordInput()->label(false);
+        ?>
         <div style="color:#999;margin:1em 0">
             If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
         </div>
